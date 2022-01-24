4e3d87b923094372e5e8f279a9eafdd2d8d0ca4e
abidh8820 <38831382+abidh8820@users.noreply.github.com>
2022-01-24 18:37:46 +0600

    Update template-mat_expo.cpp

--- template-mat_expo.cpp
+++ template-mat_expo.cpp
@@ -1,30 +1,42 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+
 const int M = 1e9 + 7;
+typedef long long LL;
+
+int n, m, K;
 
 struct Mat {
-    int a[2][2]={{0,0},{0,0}};
-    Mat operator *(const Mat& other) {
-        Mat res;
-        for (int i = 0; i < 2; i++)
-            for (int j = 0; j < 2; j++)
-                for (int k = 0; k < 2; k++) {
-                    res.a[i][k] = (res.a[i][k] + a[i][j]* other.a[j][k]) % M;
+    bool a[55][55];
+    Mat() { memset(a, 0, sizeof a);}
+
+    Mat(int x) {
+        memset(a, 0, sizeof a);
+        for (int i = 1; i <= n; i++) a[i][i] = 1;
+    }
+
+    Mat operator*(const Mat& b) {
+        Mat ret;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    ret.a[i][k] = (ret.a[i][k] + (a[i][j] * b.a[j][k]));
                 }
-        return res;
+            }
+        }
+        return ret;
     }
 };
 
-Mat iden;
-
-Mat bm(Mat a, int p) {
-    if(p==0)return iden;
+Mat bm(Mat a, LL p) {
+    if (p == 0) return Mat(1);
     if (p == 1) return a;
-    if (p & 1) return a * (bm(a, p - 1));
-    Mat tepm = bm(a, p / 2);
-    return (tepm * tepm);
+    if (p & 1) return a * bm(a, p - 1);
+    Mat temp = bm(a, p / 2);
+    return temp * temp;
 }
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
