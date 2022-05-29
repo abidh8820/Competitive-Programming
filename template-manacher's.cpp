631901c1a8d06d957a1b6f3f7f155e12dc3d16ae
Abid Hasan <38831382+abidh8820@users.noreply.github.com>
2022-05-29 11:45:16 +0600

    Update template-manacher's.cpp

--- template-manacher's.cpp
+++ template-manacher's.cpp
@@ -10,7 +10,7 @@ int main() {
 
     int n = s.size();
     vector<int> d1(n);
-    // number of palindromes taking ith as center
+    // d[i] = number of palindromes taking s[i] as center
     for (int i = 0, l = 0, r = -1; i < n; i++) {
         int k = (i > r) ? 1 : min(d1[l + r - i], r - i + 1);
         while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) k++;
@@ -18,7 +18,7 @@ int main() {
     }
 
     vector<int> d2(n);
-    // number of palindromes taking i-1 and i as center
+    // d[i] = number of palindromes taking s[i-1] and s[i] as center
     for (int i = 0, l = 0, r = -1; i < n; i++) {
         int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
         while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) k++;
