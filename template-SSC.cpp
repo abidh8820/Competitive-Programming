453e3eb7252b69e0db2d03a79ac93cf5d4435e61
Abid Hasan <38831382+abidh8820@users.noreply.github.com>
2022-05-29 12:46:58 +0600

    Update template-SSC.cpp

--- template-SSC.cpp
+++ template-SSC.cpp
@@ -1,17 +1,24 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-const int N = 1e6 + 7;
+typedef long long LL;
+const LL N = 1e6 + 7;
 
 bool vis[N];
+
+vector<int> adj[N], adjr[N];
 vector<int> order, component;
 
+// tp = 0 ,finding topo order
+// tp = 1 , reverse edge traversal
+//
 void dfs(int u, int tp = 0) {
     vis[u] = true;
-    if (tp) component.push_back(v);
-    for (int v : adj[u])
+    if (tp) component.push_back(u);
+    auto& ad = (tp ? adjr : adj);
+    for (int v : ad[u])
         if (!vis[v]) dfs(v, tp);
-    if (!tp) order.push_back(v);
+    if (!tp) order.push_back(u);
 }
 
 int main() {
@@ -19,23 +26,27 @@ int main() {
     cin.tie(0);
 
     int n, m;
-    cin >> n, m;
+    cin >> n >> m;
 
     while (m--) {
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
+        adjr[v].push_back(u);
     }
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) {
         if (!vis[i]) dfs(i);
-
-    memset(vis, 0, sizeof vi);
-    for (int i : order) {
-        if (!vis[i]) dfs(i, 1);
-        //got a new component
-        component.clear();
     }
 
+    memset(vis, 0, sizeof vis);
+    reverse(order.begin(), order.end());
+
+    for (int i : order) {
+        if (!vis[i]) {
+            // one component is found
+            dfs(i, 1), component.clear();
+        }
+    }
 }
 
