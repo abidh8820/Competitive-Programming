5d4f5aefcf8c147166c79b164ab51969296ee2d8
abid <abidh8820@gmial.com>
2022-06-26 23:48:26 +0600

    foldering

--- notes.cpp
+++
@@ -1,4 +0,0 @@
-// finding the nearest equal or greater value
-auto y = lower_bound(a.begin(), a.end(), x);
-// finding the nearest equal or lesser value
-auto y = --upper_bound(a.begin(), a.end(), x);
