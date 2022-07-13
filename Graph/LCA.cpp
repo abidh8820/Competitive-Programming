b120cc9382071e4f6b9301e996f9b30511793420
Abid Hasan <38831382+abidh8820@users.noreply.github.com>
2022-07-13 12:19:22 +0600

    Update LCA.cpp

--- Graph/LCA.cpp
+++ Graph/LCA.cpp
@@ -1,38 +1,37 @@
 #include "bits/stdc++.h"
 using namespace std;
-
-const int N = 1e5 + 7;
+ 
+const int N = 1e6 + 7, K = 22;
 vector<int> adj[N];
-int depth[N], up[N][20];
-
+int depth[N], up[N][K];
+ 
 void dfs(int u, int p) {
     for (auto v : adj[u]) {
         if (v == p) continue;
         depth[v] = depth[u] + 1;
         up[v][0] = u;
-
-        for (int i = 1; i < 20; i++) {
+ 
+        for (int i = 1; i < K; i++) {
             up[v][i] = up[up[v][i - 1]][i - 1];
         }
         dfs(v, u);
     }
 }
-
-
+ 
+ 
 int lca(int a, int b) {
     if (depth[a] < depth[b]) swap(a, b);
-    int k = depth[a] - depth[b];
-
-    for (int j = 19; j >= 0; j--) {
-        if (k & (1 << j))  a = up[a][j];
+    int g = depth[a] - depth[b];
+ 
+    for (int j = K-1; j >= 0; j--) {
+        if (g & (1 << j))  a = up[a][j];
     }
-    if (a == b)  return up[a][0];
-
-    for (int j = 19; j >= 0; j--) {
+    if (a == b)  return a;
+ 
+    for (int j = K-1; j >= 0; j--) {
         if (up[a][j] != up[b][j]) {
             a = up[a][j];  b = up[b][j];
         }
     }
     return up[a][0];
 }
-
