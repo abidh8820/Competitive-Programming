388225c9643959783dc58daef67d391cf1e39d81
abid <abidh8820@gmial.com>
2022-07-16 15:28:13 +0600

    mobius

---
+++ Math/Mobius.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+const int N = 1e6 + 5;
+int mob[N];
+
+void mobius() {
+    memset(mob, -1, sizeof mob);
+    mob[1] = 1;
+    for (int i = 2; i < N; i++) if (mob[i]){
+        for (int j = i + i; j < N; j += i) {
+                mob[j] -= mob[i];
+        }
+    }
+}
+
