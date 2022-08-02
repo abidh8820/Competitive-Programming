eb37aedd34246283fc237a6bad5672e8e08ece67
abid <abidh8820@gmial.com>
2022-08-02 16:15:13 +0600

    lca

--- Graph/LCA.cpp
+++ Graph/LCA.cpp
@@ -1,37 +1,60 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
- 
-const int N = 1e6 + 7, K = 22;
+
+const int N = 3e5 + 7, K = 20;
 vector<int> adj[N];
-int depth[N], up[N][K];
- 
-void dfs(int u, int p) {
-    for (auto v : adj[u]) {
-        if (v == p) continue;
-        depth[v] = depth[u] + 1;
-        up[v][0] = u;
- 
-        for (int i = 1; i < K; i++) {
-            up[v][i] = up[up[v][i - 1]][i - 1];
-        }
+
+int anc[N][K];
+int level[N];
+
+void dfs(int u, int par) {
+    level[u] = level[par] + 1;
+    anc[u][0] = par;
+    for (int k = 1; k < K; k++) anc[u][k] = anc[anc[u][k - 1]][k - 1];
+
+    for (int v : adj[u]) {
+        if (v == par) continue;
         dfs(v, u);
     }
 }
- 
- 
-int lca(int a, int b) {
-    if (depth[a] < depth[b]) swap(a, b);
-    int g = depth[a] - depth[b];
- 
-    for (int j = K-1; j >= 0; j--) {
-        if (g & (1 << j))  a = up[a][j];
+
+int lca(int u, int v) {
+    if (level[u] > level[v]) swap(u, v);
+    for (int k = K - 1; k >= 0; k--)
+        if (level[u] + (1 << k) <= level[v]) v = anc[v][k];
+
+    if (u == v) return u;
+    for (int k = K - 1; k >= 0; k--)
+        if (anc[u][k] != anc[v][k]) u = anc[u][k], v = anc[v][k];
+    return anc[u][0];
+}
+
+int getanc(int u, int d) {
+    for (int k = 0; k < K; k++)
+        if (d & (1 << k)) u = anc[u][k];
+    return u;
+}
+
+int dist(int u, int v) {
+    int g = lca(u, v);
+    return level[u] + level[v] - 2 * level[g];
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    if (a == b)  return a;
- 
-    for (int j = K-1; j >= 0; j--) {
-        if (up[a][j] != up[b][j]) {
-            a = up[a][j];  b = up[b][j];
-        }
-    }
-    return up[a][0];
+
+    dfs(1, 0);
+
 }
