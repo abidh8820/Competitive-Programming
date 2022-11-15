e198a05e18b737748e1ac865d1b1d10045ec1e79
Abid Hasan <abidh8820@gmail.com>
2022-11-15 17:59:53 +0600

    MCMF

--- Math/Combi.cpp
+++ Math/Combi.cpp
@@ -1,12 +1,8 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 typedef long long LL;
 
 const int N = 1e6 + 7;
-
-
-struct  MODSPACE {
-
 const int M = 1e9 + 7;
 LL fac[N], invfac[N];
 
@@ -24,8 +20,21 @@ void pre() {
 	invfac[N - 1] = BM(fac[N - 1], M - 2);
 	for (int i = N - 2; i >= 0; i--) invfac[i] = (invfac[i + 1] * (i + 1)) % M;
 }
- 
+
+LL C(int n, int r){
+    if(!fac[0])  pre();
+    if(r>n) return 0;
+    LL ret = fac[n];
+    ret = (ret * invfac[r])%M;
+    ret = (ret * invfac[n-r])%M;
+    return ret;
+}
 
 int main(){
 
+    int n, m;
+    cin >> n >> m;
+
+    if(m>=n) cout<<C(m,n)<<endl;
+    else cout<<C(n-1, m-1)<<endl;
 }
